fe93932dfa54d9ca0981e1b4f383e3bd16f1fbbd
Thomas Cholak <63080803+ThomasCholak@users.noreply.github.com>
2023-12-23 22:38:54 -0600

    trouble working with large numbers

--- Day5/Part1-Solution.py
+++ Day5/Part1-Solution.py
@@ -1,7 +1,34 @@
-def parse_int(number):
+def parse_int(number):  # parse integers into a list
     num_list = list(map(int, number.split()))
     return num_list
 
+def create_Key(plant_index):
+    xvalues = []
+    yvalues = []
+
+    for i in range(len(plant_index)):
+        repeat_num = plant_index[i][2]
+        range_start = plant_index[i][1]
+        dest_start = plant_index[i][0]
+
+        for x in range(repeat_num):
+            xvalues.append(range_start + x)
+            yvalues.append(dest_start + x)
+        else:
+            continue
+
+    return xvalues, yvalues
+
+def new_Seeds(seeds, xvalues, yvalues):
+    for j in range(len(seeds)):
+        valid = seeds[j] in xvalues
+
+        if valid:
+            key = xvalues.index(seeds[j])
+            seeds[j] = yvalues[key]
+        else:
+            continue
+    return seeds
 
 if __name__ == "__main__":
     # have to initialize lists for storing mapping data
@@ -10,7 +37,7 @@ if __name__ == "__main__":
     soil_to_fert = []
     fert_to_water = []
     water_to_light = []
-    light_to_temperature = []
+    light_to_temp = []
     temp_to_humid = []
     humid_to_loc = []
 
@@ -21,7 +48,6 @@ if __name__ == "__main__":
 
         res_1 = temp[0].replace("seeds: ", "")  # removes 'seeds: ' from beginning of string
         seed_List = parse_int(res_1)  # parse first line into integer list
-        # print(seedList)
 
         if 2 < i < 19:
             temp_list = parse_int(temp[i])  # seed-to-soil
@@ -37,7 +63,7 @@ if __name__ == "__main__":
             water_to_light.append(temp_list)
         elif 107 < i < 134:
             temp_list = parse_int(temp[i])  # light to temperature
-            light_to_temperature.append(temp_list)
+            light_to_temp.append(temp_list)
         elif 135 < i < 158:
             temp_list = parse_int(temp[i])  # temperature to humidity
             temp_to_humid.append(temp_list)
@@ -47,11 +73,19 @@ if __name__ == "__main__":
         else:
             continue
 
+    table_values = [
+        seed_to_soil,
+        soil_to_fert,
+        fert_to_water,
+        water_to_light,
+        light_to_temp,
+        temp_to_humid,
+        humid_to_loc
+    ]
+
+    for val in table_values:
+        x_values, y_values = create_Key(table_values)
+        seed_List = new_Seeds(seed_List, x_values, y_values)
+
     print(seed_List)
-    print(seed_to_soil)
-    # print(soil_to_fert)
-    # print(fert_to_water)
-    # print(water_to_light)
-    # print(light_to_temperature)
-    # print(temp_to_humid)
-    # print(humid_to_loc)
+    
